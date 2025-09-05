819fdd4c5023c2f00bc189ccee79ceeb4a543365
Madeleine <madelienesucon20@gmail.com>
2025-09-05 09:53:51 -0500

    SUBIENDO EJER1 DE SEMANA3

---
+++ SEMANA1_PY/SEMANA1_PY.py
@@ -0,0 +1,8 @@
+# PROYECTO DE SEMANA 03 
+def ejer1():
+    NOMBRE = input("INGRSE SU NOMBRE:")
+    CARRERA = input("INGRESE SU CARRERA:")
+
+    print(f"\n TU NOMBRE ES {NOMBRE} BIENVENIDO(A) A TU PRIMERA CLASE DE {CARRERA}")
+
+ejer1()
---
+++ Semana01_c#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana01_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey(); // Realizando detenimiento de comsola
+
+        }
+
+        static void ejer1()
+        {
+            String nombre, carrera; 
+
+            Console.Write("INGRESE SU NOMBRE:");
+            nombre = Console.ReadLine();
+
+            Console.Write("INGRESE SU CARRERA:");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n TU NOMBRE ES {nombre} BIENVENIDO A TU PRIMERA CLASE DE {carrera}");
+
+        }
+
+    }
+}
